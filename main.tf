7c313fc93f265768e9011349f806bfde359d34d4
kaliyappan R <Kaliyappan>
2025-02-05 18:24:50 +0530

    SCD-I1006 - Created as spk enterprise in aws marketplace ami also assign with elastic ip was created instance

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ terraform {
       version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
@@ -12,20 +14,47 @@ provider "aws" {
 }
 
 resource "aws_instance" "splunk_serverr" {
-  ami           = "ami-07a79552200256856"
+  ami           = "ami-08f75f3652f7d91ef"
   instance_type = "t2.medium"
+  associate_public_ip_address = true
   key_name = var.key_name
-
+  vpc_security_group_ids = ["${aws_security_group.splunk_serverr.id}"]
   tags = {
     Name = "splunk-server"
   }
 
 }
 
+resource "aws_eip" "splunk_eip" {
+  instance = aws_instance.splunk_serverr.id
+}
+
+
+resource "aws_security_group" "splunk_serverr" {
+  name_prefix = "splunk-group"
+
+  ingress {
+    from_port = 22
+    to_port  = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port = 8000
+    to_port  = 8000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 variable "key_name" {
   type = string
   
 }
+
+
 
 output "Splunk_Instance_Name" {
   value = aws_instance.splunk_serverr.tags["Name"]
@@ -43,7 +72,7 @@ output "Instance_State" {
   value = aws_instance.splunk_serverr.instance_state
 }
 output "Splunk_SSH_String" {
-  value = "ssh -i ${var.key_name}.pem ec2-user@${aws_instance.splunk_serverr.public_dns}"
+  value = "ssh -i ${var.key_name}.pem ec2-user@${aws_eip.splunk_eip.public_dns}"
 }
 
 
