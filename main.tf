fa721446f394de5fddeacb72d709f31b2cf17126
kaliyappan R <Kaliyappan>
2025-02-07 19:30:27 +0530

    SCD-I1006 - The committed code provisions individual instances with specific details, including Elastic IP and security settings, managed via terraform.tfvars

--- main.tf
+++ main.tf
@@ -19,69 +19,59 @@ variable "region" {
 }
 
 # Variable for AMI ID
-variable "splunk_ami_id" {
-  type        = string
-  description = "AMI ID for Splunk Enterprise"
+variable "ami_map" {
+  type        = map(string)
+  description = "AMI ID mapping for each region"
+  default = {
+    "ap-southeast-1"  = "ami-08f75f3652f7d91ef"
+    "us-east-2"       = "ami-092484aa52514c4fd"
+    "us-west-2"       = "ami-0f59e596bfab2d9cf"
+    "us-east-1"       = "ami-09041c38b0523b954"
+    "us-west-1"       = "ami-0f6eefa9bcd20c6fe"
+    "ap-south-1"      = "ami-0ce2b421400012344"
+    "ap-southeast-2"  = "ami-073d1f71ff3f1b969"
+    "ap-northeast-3"  = "ami-0c21205c1ccc847b2"
+    "ap-northeast-2"  = "ami-0eddf325ed0603e40"
+    "ap-northeast-1"  = "ami-03fe4f0017a618b75"
+    "ca-central-1"     = "ami-0ea202e2fd68e541a"
+    "eu-central-1"    = "ami-0b43a36b3d38be336"
+    "eu-west-1"       = "ami-079f8d6cfabe5a428"
+    "eu-west-2"       = "ami-0b6374030d6461abe"
+    "eu-west-3"       = "ami-0265048c92cbd07fa"
+    "eu-north-1"      = "ami-084f0bd811ba09257"
+  }
 }
 
 
-# Variable definitions for flexibility
-variable "instance_count" {
-  type = number
-  description = "Number of Splunk instances to create"
-  default = 1 # Default to 1 if not specified
-}
-
-variable "instance_names" {
-  type = list(string)
-  description = "List of instance names (must match instance_count)"
-  default = ["splunk-server-1"]
-}
-
-
-variable "key_name" {
-  type = string
-  description = "Name of the SSH key pair"
-}
-
-variable "elastic_ips_needed" {
-  type = bool
-  description = "Whether to associate Elastic IPs"
-  default = true
-}
-
-variable "security_group_rules" {
+# List of instance configurations
+variable "instances" {
   type = list(object({
-    from_port = number
-    to_port   = number
-    protocol  = string
-    cidr_blocks = list(string)
+
+    name = string
+    region            = string
+    instance_type     = string
+    storage_size      = number
+    key_name          = string
+    elastic_ip_needed = bool
+    security_group_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+
   }))
-  description = "List of security group ingress rules"
-  default = [
-    { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-    { from_port = 8000, to_port = 8000, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-        { from_port = 8089, to_port = 8089, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-        { from_port = 554, to_port = 554, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-        { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-        { from_port = 9997, to_port = 9997, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-  ]
+  description = "List of Splunk instances with individual configurations."
 }
 
-variable "storage_size" {
-  type = number
-  description = "Size of the root volume in GB"
-  default = 30 
-}
+# Security Group Creation 
+resource "aws_security_group" "splunk_sg" {
+  for_each = { for idx, instance in var.instances : idx => instance }
 
-
-# Security group
-resource "aws_security_group" "splunk_server_sg" {
-  count = var.instance_count
-  name_prefix = "splunk-sg-"
+  name_prefix = "splunk-sg-${each.value.name}"
 
   dynamic "ingress" {
-    for_each = var.security_group_rules
+    for_each = each.value.security_group_rules
     content {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
@@ -89,7 +79,6 @@ resource "aws_security_group" "splunk_server_sg" {
       cidr_blocks = ingress.value.cidr_blocks
     }
   }
-
 
   egress {
     from_port = 0
@@ -99,82 +88,80 @@ resource "aws_security_group" "splunk_server_sg" {
   }
 }
 
-variable "instance_type" {
-    type = string
-    description = "Enter the instance type for Splunk servers (Default: t2.medium)"
-    default = "t2.medium"
-}
-
-
 # Splunk instances
 resource "aws_instance" "splunk_server" {
-  count = var.instance_count
-  ami           = var.splunk_ami_id
-  instance_type = var.instance_type
-  key_name = var.key_name
-  vpc_security_group_ids = [aws_security_group.splunk_server_sg[count.index].id]
-  associate_public_ip_address = !var.elastic_ips_needed
+  for_each = { for idx, instance in var.instances : idx => instance }
+
+  provider = aws
+  ami           = lookup(var.ami_map, each.value.region, "")
+  instance_type = each.value.instance_type
+  key_name = each.value.key_name
+  vpc_security_group_ids = [aws_security_group.splunk_sg[each.key].id]
+  associate_public_ip_address = each.value.elastic_ip_needed
 
   root_block_device {
-    volume_size = var.storage_size
+    volume_size = each.value.storage_size
   }
 
   tags = {
-    Name = var.instance_names[count.index] # Use the provided names list
+    Name = each.value.name
   }
 }
 
 
 # Elastic IPs (only if needed)
 resource "aws_eip" "splunk_eip" {
-  count = var.elastic_ips_needed ? var.instance_count : 0
-  instance = aws_instance.splunk_server[count.index].id
+  for_each = { for idx, instance in var.instances : idx => instance if instance.elastic_ip_needed }
+
+  instance = aws_instance.splunk_server[each.key].id
   vpc = true
 }
 
 
 # Outputs (updated for multiple instances)
 output "splunk_instance_names" {
-  value = aws_instance.splunk_server[*].tags["Name"]
+  value = {
+    for idx, instance in aws_instance.splunk_server : instance.tags["Name"] => instance.id
+  }
 }
 
 output "splunk_public_ips" {
   value = { 
-    for idx, instance in aws_instance.splunk_server[*] :
-    instance.tags["Name"] => var.elastic_ips_needed ? aws_eip.splunk_eip[idx].public_ip : instance.public_ip
+    for idx, instance in aws_instance.splunk_server :
+    instance.tags["Name"] => (lookup(aws_eip.splunk_eip, idx, null) !=null ? aws_eip.splunk_eip[idx].public_ip : instance.public_ip)
   }
 }
 
 output "splunk_private_ips" {
   value = {
-    for idx, instance in aws_instance.splunk_server[*] : 
+    for idx, instance in aws_instance.splunk_server : 
     instance.tags["Name"] => instance.private_ip
   }
 }
 
 output "instance_states" {
   value = {
-    for idx, instance in aws_instance.splunk_server[*] : 
+    for idx, instance in aws_instance.splunk_server : 
     instance.tags["Name"] => instance.instance_state
   }
 }
 
 output "splunk_ssh_strings" {
   value ={
-    for idx, instance in aws_instance.splunk_server[*] :
-    instance.tags["Name"] => "ssh -i ${var.key_name}.pem ec2-user@${instance.public_dns}"
+    for idx, instance in aws_instance.splunk_server :
+    instance.tags["Name"] => "ssh -i ${var.instances[idx].key_name}.pem ec2-user@${instance.public_dns}"
   } 
 }
 
 output "splunk_passwords" {
   value = {
-    for idx, instance in aws_instance.splunk_server[*] : 
+    for idx, instance in aws_instance.splunk_server : 
     instance.tags["Name"]  => "SPLUNK-${instance.id}"
   } 
 }
 
 output "splunk_usernames" {
  value = {
-    for idx, instance in aws_instance.splunk_server[*] : instance.tags["Name"] => "admin"
+    for idx, instance in aws_instance.splunk_server : instance.tags["Name"] => "admin"
  } 
 }
